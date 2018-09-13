8ba1bc59db9020680a17c0407d2d5483aa27312f
Roman Valihura <roman.valihura@gmail.com>
2018-09-13 16:56:01 +0300

    downgrade fluture wersion for types copability

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,14 @@
-import { FutureInstance } from 'fluture'
+import { Future } from 'fluture'
 
 declare module 'fluture-retry' {
   export type RetryStrategy = (initialRetryTime: number) => number
 
-  export function retryLinearly <E, R>(task: FutureInstance<E, R>): FutureInstance<E, R>
+  export function retryLinearly <E, R>(task: Future<E, R>): Future<E, R>
 
-  export function retry (strategy: RetryStrategy): (countTries: number) => <E, R>(task: FutureInstance<E, R>) => FutureInstance<E[], R>
-  export function retry (strategy: RetryStrategy, countTries: number): <E, R>(task: FutureInstance<E, R>) => FutureInstance<E[], R>
-  export function retry (strategy: RetryStrategy): (countTries: number) => <E, R>(task: FutureInstance<E, R>) => FutureInstance<E[], R>
-  export function retry <E, R>(strategy: RetryStrategy, countTries: number, task: FutureInstance<E, R>): FutureInstance<E[], R>
+  export function retry (strategy: RetryStrategy): (countTries: number) => <E, R>(task: Future<E, R>) => Future<E[], R>
+  export function retry (strategy: RetryStrategy, countTries: number): <E, R>(task: Future<E, R>) => Future<E[], R>
+  export function retry (strategy: RetryStrategy): (countTries: number) => <E, R>(task: Future<E, R>) => Future<E[], R>
+  export function retry <E, R>(strategy: RetryStrategy, countTries: number, task: Future<E, R>): Future<E[], R>
 
   export function exponentially (multiplier: number): RetryStrategy
   export function exponentially (multiplier: number, initialRetryTime: number): number
